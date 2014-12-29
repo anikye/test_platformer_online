d2f835f463e7518abeff78ca8077535f28560097
Nik MSI <wisitnik@gmail.com>
2014-12-29 17:16:11 +0700

    implement basic player control

---
+++ Assets/_PROJECT/PlayerController2D.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController2D : MonoBehaviour
+{
+    private Vector2 _movement;
+    private float horizontal;
+    private Rigidbody2D _rigid;
+
+    public float RunSpeed = 5;
+    public ProxyTrigger OnGround;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        _movement = new Vector2(horizontal * RunSpeed, 0) * Time.fixedDeltaTime;
+        if (OnGround.IsContract)
+        {
+
+        }
+        else
+        {
+            _movement.y += Physics2D.gravity.y * _rigid.gravityScale * Time.fixedDeltaTime;
+        }
+        
+        _rigid.MovePosition(_rigid.position + _movement);
+        
+    }
+}
---
+++ Assets/_PROJECT/ProxyTrigger.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProxyTrigger : MonoBehaviour
+{
+    public List<Collider2D> ContractList;
+    public List<string> Tags;
+
+    public bool IsContract
+    {
+        get { return ContractList.Count > 0; }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < ContractList.Count; i++)
+        {
+            if (ContractList[i] == null)
+            {
+                ContractList.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Tags.Count > 0)
+        {
+            for (int i = 0; i < Tags.Count; i++)
+            {
+                if (other.CompareTag(Tags[i]))
+                {
+                    ContractList.Add(other);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            ContractList.Add(other);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (ContractList.Contains(other))
+        {
+            ContractList.Remove(other);
+        }
+    }
+}
